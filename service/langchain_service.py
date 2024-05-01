3f5508c71d461fd9293f116e878f51bc3cbf4bf9
Salva Roca <salva.roca@icloud.com>
2024-05-01 17:52:16 +0200

    feat: Multi-PDF load

--- service/langchain_service.py
+++ service/langchain_service.py
@@ -1,4 +1,4 @@
-import dotenv
+import os, dotenv
 from langchain_community.llms import HuggingFaceEndpoint
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import CharacterTextSplitter
@@ -16,8 +16,7 @@ def format_docs(docs):
 def load_rag_chain(repo_id):
     dotenv.load_dotenv()
 
-    loader = PyPDFLoader("data/esl-ES-2.pdf")
-
+    # Definir el objeto TextSplitter
     text_splitter = CharacterTextSplitter(
         separator="\n\n",
         chunk_size=1000,
@@ -26,15 +25,32 @@ def load_rag_chain(repo_id):
         is_separator_regex=False,
     )
 
-    chunks = loader.load_and_split(text_splitter=text_splitter)
+    # Lista para almacenar los chunks de texto de todos los archivos PDF
+    all_chunks = []
+
+    # Iterar a través de cada archivo en la carpeta 'data'
+    for filename in os.listdir('data'):
+        # Verificar si el archivo es un PDF
+        if filename.endswith('.pdf'):
+            # Construir la ruta relativa del archivo PDF
+            pdf_path = os.path.join('data', filename)
+
+            # Cargar el archivo PDF
+            loader = PyPDFLoader(pdf_path)
+
+            # Dividir el archivo PDF en chunks de texto
+            chunks = loader.load_and_split(text_splitter=text_splitter)
+
+            # Agregar los chunks de texto a la lista
+            all_chunks.extend(chunks)
 
     embeddings = HuggingFaceInstructEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",
                                                model_kwargs={'device': "cpu"})
 
-    vectorstore = FAISS.from_documents(documents=chunks, embedding=embeddings)
+    vectorstore = FAISS.from_documents(documents=all_chunks, embedding=embeddings)
     retriever = vectorstore.as_retriever()
 
-    llm = HuggingFaceEndpoint(repo_id=repo_id, temperature=0.5, model_kwargs={"max_length": 512})
+    llm = HuggingFaceEndpoint(repo_id=repo_id, temperature=0.3)
 
     prompt = hub.pull("rlm/rag-prompt")
 
